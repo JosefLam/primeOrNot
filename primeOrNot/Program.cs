6f9f62ec5eb9b4d114c429f51b201cce381ea0a2
Josef Lambley <17LamblJ@fulford.york.sch.uk>
2022-11-23 12:39:58 +0000

    started

--- primeOrNot/Program.cs
+++ primeOrNot/Program.cs
@@ -10,6 +10,13 @@ namespace primeOrNot
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("enter start number");
+            int min = 0;
+            int.TryParse(Console.ReadLine(), out min);
+
+            Console.WriteLine("enter end number");
+            string endNum = Console.ReadLine();
+
         }
     }
 }
